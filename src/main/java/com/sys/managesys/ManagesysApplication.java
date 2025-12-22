8c4b91079cbcc6f3aadfa656452dc5c1e4f951e0
sanghee yang <tkdgmlek567@gmail.com>
2025-12-22 16:29:29 +0900

    Initial commit

---
+++ src/main/java/com/sys/managesys/ManagesysApplication.java
@@ -0,0 +1,13 @@
+package com.sys.managesys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagesysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagesysApplication.class, args);
+	}
+
+}
